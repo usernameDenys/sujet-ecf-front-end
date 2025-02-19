34375543f090d75b295123d0270bcedb95685521
Denys <denysgolenko@gmail.com>
2025-02-19 16:27:25 +0100

    navmenu upd

--- webfiles/js/hanburger_menu.js
+++ webfiles/js/hanburger_menu.js
@@ -1,10 +1,12 @@
 const hamburgerMenu = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const links = document.querySelectorAll(".nav-link");
+const intro = document.querySelector("#intro");
 
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("active");
   navMenu.classList.toggle("active");
+  intro.style.marginTop = "250px";
 });
 
 links.forEach((element) =>
